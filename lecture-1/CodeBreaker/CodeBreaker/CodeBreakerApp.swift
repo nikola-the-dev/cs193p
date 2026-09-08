ade495ed68d6f828c0354d0f16e79dc604484323
Nick Voloshyn <nikaladze@icloud.com>
2026-09-08 15:18:15 +0300

    First lecture: introduction

---
+++ lecture-1/CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Nick Voloshyn on 08.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
