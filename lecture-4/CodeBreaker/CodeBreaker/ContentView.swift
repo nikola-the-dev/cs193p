d6fc537efb9d11b9b286b492c629d1d20552f8d6
Mykola Voloshyn <nikaladze@icloud.com>
2026-09-19 14:22:49 +0300

    Some cleans

--- lecture-4/CodeBreaker/CodeBreaker/ContentView.swift
+++ lecture-4/CodeBreaker/CodeBreaker/ContentView.swift
@@ -16,30 +16,11 @@ struct ContentView: View {
         }
         .padding()
     }
-        
-//    Then we can call this variable in component just like this:
-//    VStack {
-//        pegs
-//        pegs
-//        pegs
-//    }
-    var pegs: some View {
-        HStack {
-            Circle().foregroundStyle(.red)
-//            foregroundStyle modifier is more powerfull than foregroundColor
-            Circle().foregroundStyle(.red)
-            Circle().foregroundStyle(.red)
-            Circle().foregroundStyle(.green)
-        }
-    }
-    
+
     
     func pegs(colors: [Color]) -> some View {
         HStack {
             ForEach(colors.indices, id: \.self) { i in
-//                id field in this call needed mostly for animation because system need to know
-//                which and how update every element, this field should be UNIQUE
-//                \.self in this case means that this concret object responds as id
                 RoundedRectangle(cornerRadius: 10.0)
                     .aspectRatio(1/1, contentMode: .fit)
                     .foregroundStyle(colors[i])
